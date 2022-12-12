e184a97f9fc3a2e3bebd6a51b263b5e3d2a03ecd
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:17:18 +0530

    Auto Push Commit

--- peerintg.tf
+++ peerintg.tf
@@ -1,9 +1,8 @@
 resource "aws_vpc_peering_connection" "peer" {
-  peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.bar.id
+  peer_vpc_id   = aws_vpc.main.id            # VPC that we are creating
   vpc_id        = aws_vpc.foo.id
 
   tags = {
-    Name  = ${var.ENV}-default-robot-peering
+    Name  = "${var.ENV}-default-robot-peering"
   }
 }
