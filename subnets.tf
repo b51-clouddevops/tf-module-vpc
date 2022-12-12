7a7f73f1e5dcb3649d45e6d0fc2b607095a2eeff
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:58:45 +0530

    asdf

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,11 @@
 # count.index is going to fetch the count iteration and element is going to fetch the value from the list as per the index
 # count and element are funcitons in terraform
 resource "aws_subnet" "public" {
-  count                 = length(var.PUBLIC_SUBNET_CIDR) 
-  vpc_id                = aws_vpc.main.id
-  cidr_block            = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone     = element(var.AZ, count.index)
+  count                    = length(var.PUBLIC_SUBNET_CIDR) 
+  vpc_id                   = aws_vpc.main.id
+  cidr_block               = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone        = element(var.AZ, count.index)
+  map_public_ip_on_launch  = true
 
   tags = {
     Name = "${var.ENV}-pub-${element(var.AZ, count.index)}"
