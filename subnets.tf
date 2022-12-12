cce21a2321a870f623a5d8bfd701af0945d5a3f7
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:03:57 +0530

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,13 +1,11 @@
 # count.index is going to fetch the count iteration and element is going to fetch the value from the list as per the index
-
+# count and element are funcitons in terraform
 resource "aws_subnet" "public" {
   count      = length(var.PUBLIC_SUBNET_CIDR) 
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "Main"
+    Name = "robot-${var.env}-public-subnet"
   }
 }
-
-# count and element are funcitons in terraform
