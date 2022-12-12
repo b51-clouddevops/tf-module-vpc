3f104215a8fb3c718cba7dc3070d5af4e868d08e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:44:06 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -2,7 +2,6 @@
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
 
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -19,3 +18,7 @@ resource "aws_route_table" "public-rt" {
 }
 
 # Attach the route table to public subnets
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.foo.id
+  route_table_id = aws_route_table.bar.id
+}
