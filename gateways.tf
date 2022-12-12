75d10d98d0cc0a329bd23e1fea6b79ef6ebaf0e7
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:29:37 +0530

    Auto Push Commit

--- gateways.tf
+++ gateways.tf
@@ -9,7 +9,7 @@ resource "aws_internet_gateway" "igw" {
 
 
 # Creates NAT Gateway
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.example.id
   subnet_id     = aws_subnet.example.id
 
@@ -17,7 +17,5 @@ resource "aws_nat_gateway" "example" {
     Name = "gw NAT"
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.example]
 }
