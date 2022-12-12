e144bc15d95e8adef854e73fc86eac56e9970698
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:09:47 +0530

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 # count.index is going to fetch the count iteration and element is going to fetch the value from the list as per the index
 # count and element are funcitons in terraform
 resource "aws_subnet" "public" {
-  count      = length(var.PUBLIC_SUBNET_CIDR) 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  count                 = length(var.PUBLIC_SUBNET_CIDR) 
+  vpc_id                = aws_vpc.main.id
+  cidr_block            = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone     = 
 
   tags = {
     Name = "${var.ENV}-pub-${element(var.AZ, count.index)}"
@@ -13,9 +14,10 @@ resource "aws_subnet" "public" {
 
 # Creates private subnets
 resource "aws_subnet" "private" {
-  count      = length(var.PRIVATE_SUBNET_CIDR) 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  count                 = length(var.PRIVATE_SUBNET_CIDR) 
+  vpc_id                = aws_vpc.main.id
+  cidr_block            = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone     = 
 
   tags = {
     Name = "${var.ENV}-prv-${element(var.AZ, count.index)}"
