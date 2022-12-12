689bd19f96384efd088092c2318abafbd38ec813
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:40:07 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -1,8 +1,8 @@
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.example.id
   }
 
