bf5dd3754928053a1d3f6b042726669c4df86ba1
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:25:23 +0530

    Auto Push Commit

--- peerintg.tf
+++ peerintg.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id            # VPC that we are creating
   vpc_id        = var.DEFAULT_VPC_ID         # Default VPC ID, where our bastion, WS jenkins are residing.
-  auto_accept   = true
+  auto_accept   = true                       # Only works if both the VPC are in same region and are of same account
   tags = {
     Name  = "${var.ENV}-default-robot-peering"
   }
