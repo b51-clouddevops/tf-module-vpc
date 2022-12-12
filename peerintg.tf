c1f0a82b55f795f50d286a967dc003394027a91e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:20:53 +0530

    asdf

--- peerintg.tf
+++ peerintg.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id            # VPC that we are creating
-  vpc_id        =              # Default VPC ID, where our bastion, WS jenkins are residing.
+  vpc_id        = var.DEFAULT_VPC_ID         # Default VPC ID, where our bastion, WS jenkins are residing.
 
   tags = {
     Name  = "${var.ENV}-default-robot-peering"
