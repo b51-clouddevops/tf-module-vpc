7aed14cd26881c6bd449a7192ed57568a63fa68f
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:47:20 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -1,4 +1,4 @@
-# Creates Route Table
+# Creates Public Route Table
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
 
@@ -21,5 +21,31 @@ resource "aws_route_table" "public-rt" {
 resource "aws_route_table_association" "a" {
   count          = length(aws_subnet.public.*.id) 
   subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.bar.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# Creates Private Route Table
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "${var.ENV}-public-route-table"
+  }
+}
+
+# Attach the route table to public subnets
+resource "aws_route_table_association" "a" {
+  count          = length(aws_subnet.public.*.id) 
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
 }
