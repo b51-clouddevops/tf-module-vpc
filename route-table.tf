e3294fd185e6a761ce8ebd66bf550e6ae9aad750
mvpc-11232 <mvp2612@hotmail.com>
2022-12-12 08:50:52 +0530

    asdf

--- route-table.tf
+++ route-table.tf
@@ -44,7 +44,7 @@ resource "aws_route_table" "private-rt" {
 }
 
 # Attach the route table to public subnets
-resource "aws_route_table_association" " "prv-rt-association"" {
+resource "aws_route_table_association" "prv-rt-association" {
   count          = length(aws_subnet.private.*.id) 
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = aws_route_table.private-rt.id
