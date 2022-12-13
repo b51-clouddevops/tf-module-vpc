be1d68f2aa73b56d0555e22f0fb49629777f2eda
mvpc-11232 <mvp2612@hotmail.com>
2022-12-13 07:51:58 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -52,3 +52,9 @@ resource "aws_route_table_association" "prv-rt-association" {
 
 
 # Adding the Route in Default VPC Route Table ( which is the peering connection to robot vpc)
+resource "aws_route" "r" {
+  route_table_id            = "rtb-4fbb3ac4"
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+  depends_on                = [aws_route_table.testing]
+}
